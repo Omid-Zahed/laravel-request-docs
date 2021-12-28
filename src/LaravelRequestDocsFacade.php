f5e847648a81ba0a902861e2249e80d1da2ea313
omid-zahed <omid98.ir@gmail.com>
2021-12-28 10:59:22 +0330

    init

---
+++ src/LaravelRequestDocsFacade.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OmidZahed\LaravelRequestDocs;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Rakutentech\LaravelRequestDocs\LaravelRequestDocs
+ */
+class LaravelRequestDocsFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-request-docs';
+    }
+}
