b9995b9a6ea29f0a95f86ca48f205d9e60c78668
omidzahed <omid98.ir@gmail.com>
2022-06-07 20:42:13 +0330

    added rule for add description in requests

---
+++ src/Rules/SwaggerDoc.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OmidZahed\LaravelRequestDocs\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class SwaggerDoc implements Rule
+{
+    private $description;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return $value;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return '';
+    }
+
+    public function __toString(): string
+    {
+        return $this->description;
+    }
+}
